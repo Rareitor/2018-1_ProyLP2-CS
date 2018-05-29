b6a1c97f84f1c295d42ba6b451bd7ea8202df7a2
alulab14 <alulab14@inf.pucp.edu.pe>
2018-05-29 11:37:40 -0500

    ofjb

---
+++ GESCOM TDP/Vista/FrmVisualizarCombo.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Vista
+{
+    partial class FrmVisualizarCombo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCombo = new System.Windows.Forms.DataGridView();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnRecuperar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCombo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCombo
+            // 
+            this.dgvCombo.AllowUserToOrderColumns = true;
+            this.dgvCombo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCombo.Location = new System.Drawing.Point(43, 76);
+            this.dgvCombo.Name = "dgvCombo";
+            this.dgvCombo.Size = new System.Drawing.Size(710, 299);
+            this.dgvCombo.TabIndex = 0;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(667, 33);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 14;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // btnRecuperar
+            // 
+            this.btnRecuperar.Location = new System.Drawing.Point(531, 33);
+            this.btnRecuperar.Name = "btnRecuperar";
+            this.btnRecuperar.Size = new System.Drawing.Size(75, 23);
+            this.btnRecuperar.TabIndex = 15;
+            this.btnRecuperar.Text = "Recuperar";
+            this.btnRecuperar.UseVisualStyleBackColor = true;
+            // 
+            // FrmVisualizarCombo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 415);
+            this.Controls.Add(this.btnRecuperar);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.dgvCombo);
+            this.Name = "FrmVisualizarCombo";
+            this.Text = "FrmVisualizarCombo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCombo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCombo;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnRecuperar;
+    }
+}
