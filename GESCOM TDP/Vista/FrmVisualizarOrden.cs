b71933540565abe3b573d1726caed2d07ca9a4bd
oscar20183 <39636280+oscar20183@users.noreply.github.com>
2018-05-29 16:26:20 -0500

    clase pdf
    sd

--- GESCOM TDP/Vista/FrmVisualizarOrden.cs
+++ GESCOM TDP/Vista/FrmVisualizarOrden.cs
@@ -150,118 +150,16 @@ namespace Vista.Otros
                 excel.GenerarExcel();
                 MessageBox.Show("Se ha generado correctamente el archivo excel");
             } else if (rbnPdf.Checked == true){
-                ExportarDataGridViewPdf(dgvRecord);
+                ExportarPdf pdf = new ExportarPdf(dgvRecord);
+                pdf.ExportarDataGridViewPdf();
+                MessageBox.Show("Se ha generado correctamente el archivo pdf");
             }
             
         }
 
 
-        //Función que genera el documento Pdf 
-        public void GenerarDocumentos(Document document)
-        {
-            //se crea un objeto PdfTable con el numero de columnas del dataGridView 
-            PdfPTable datatable = new PdfPTable(dgvRecord.ColumnCount);
-
-            //asignamos algunas propiedades para el diseño del pdf  
-            datatable.DefaultCell.Padding = 1;
-            float[] headerwidths = GetTamañoColumnas(dgvRecord);
-
-            datatable.SetWidths(headerwidths);
-            datatable.WidthPercentage = 100;
-            datatable.DefaultCell.BorderWidth = 1;
-
-            //DEFINIMOS EL COLOR DE LAS CELDAS EN EL PDF 
-            datatable.DefaultCell.BackgroundColor = iTextSharp.text.BaseColor.WHITE;
-
-            //DEFINIMOS EL COLOR DE LOS BORDES 
-            datatable.DefaultCell.BorderColor = iTextSharp.text.BaseColor.BLACK;
-
-            //LA FUENTE DE NUESTRO TEXTO 
-            iTextSharp.text.Font fuente = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA);
-
-            Phrase objP = new Phrase("A", fuente);
-
-            datatable.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
-
-            //SE GENERA EL ENCABEZADO DE LA TABLA EN EL PDF  
-            for (int i = 0; i < dgvRecord.ColumnCount; i++)
-            {
-
-                objP = new Phrase(dgvRecord.Columns[i].HeaderText, fuente);
-                datatable.HorizontalAlignment = Element.ALIGN_CENTER;
-
-                datatable.AddCell(objP);
-
-            }
-            datatable.HeaderRows = 1 ; //AQUI CAMBIARE;
-
-
-            datatable.DefaultCell.BorderWidth = 1;
-
-            //SE GENERA EL CUERPO DEL PDF 
-            for (int i = 0; i < dgvRecord.RowCount-1; i++)
-            {
-                for (int j = 0; j < dgvRecord.ColumnCount; j++)
-                {
-                    objP = new Phrase(dgvRecord[j, i].Value.ToString(), fuente);
-                    datatable.AddCell(objP);
-                }
-                datatable.CompleteRow();
-            }
-            
-            document.Add(datatable);
-        }
-        
-        //Función que obtiene los tamaños de las columnas del datagridview 
-        public float[] GetTamañoColumnas(DataGridView dg)
-        {
-            //Tomamos el numero de columnas 
-            float[] values = new float[dg.ColumnCount];
-            for (int i = 0; i < dg.ColumnCount; i++)
-            {
-                //Tomamos el ancho de cada columna 
-                values[i] = (float)dg.Columns[i].Width;
-            }
-            return values;
-        }
        
 
-        private void ExportarDataGridViewPdf(DataGridView dgvRecord)
-        {
-            if (dgvRecord.RowCount == 0)
-            {
-                MessageBox.Show("No Hay Datos Para Realizar Un Reporte");
-            }
-            else
-            {    //ESCOJE A RUTA DONDE GUARDAREMOS EL PDF 
-                SaveFileDialog save = new SaveFileDialog();
-                save.Filter = "PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
-                if (save.ShowDialog() == DialogResult.OK)
-                {
-                    string filename = save.FileName;
-                    Document doc = new Document(PageSize.A3, 9, 9, 9, 9);
-                    Chunk encab = new Chunk("REPORTE", FontFactory.GetFont("COURIER", 18));
-                    try
-                    {
-                        FileStream file = new FileStream(filename, FileMode.OpenOrCreate);
-                        PdfWriter writer = PdfWriter.GetInstance(doc, file);
-                        writer.ViewerPreferences = PdfWriter.PageModeUseThumbs;
-                        writer.ViewerPreferences = PdfWriter.PageLayoutOneColumn;
-                        doc.Open();
-
-                        doc.Add(new Paragraph(encab));
-                        GenerarDocumentos(doc);
-
-                        Process.Start(filename);
-                        doc.Close();
-                    }
-
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-                }
-            }
-        }
+      
     }
 }
