8ea3236d686e5a8b31a3090290f354dd2f5015f7
oscar20183 <39636280+oscar20183@users.noreply.github.com>
2018-06-13 01:30:42 -0500

    ultimos
    yyaa

--- GESCOM TDP/Vista/FrmVisualizarProducto.Designer.cs
+++ GESCOM TDP/Vista/FrmVisualizarProducto.Designer.cs
@@ -38,6 +38,8 @@
             this.btnRecuperar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProducto)).BeginInit();
             this.SuspendLayout();
             // 
@@ -97,7 +99,7 @@
             // 
             // btnRegresar
             // 
-            this.btnRegresar.Location = new System.Drawing.Point(576, 446);
+            this.btnRegresar.Location = new System.Drawing.Point(265, 454);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(75, 23);
             this.btnRegresar.TabIndex = 13;
@@ -132,11 +134,31 @@
             this.label1.TabIndex = 16;
             this.label1.Text = "Nombre del Producto:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Menos Vendidos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(543, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Enviar Notificacion";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FrmVisualizarProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 489);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnRecuperar);
@@ -164,5 +186,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn idProducto;
         private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn tipo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
